7733e87e140cc1a85da5050b90e19862c0ca3cfd
Élie Bouttier <cube.elie@gmail.com>
2011-10-19 02:25:43 +0200

    Correction problème menu imbriqué package/Config.in

--- Buildroot/03-buildroot-tune.sh
+++ Buildroot/03-buildroot-tune.sh
@@ -1,5 +1,5 @@
 cp buildroot-config buildroot-2011.08/.config
 cp package/ipshow buildroot-2011.08/package/ -r
-cat package/Config.in >> buildroot-2011.08/package/Config.in
+cp package/Config.in buildroot-2011.08/package/Config.in
 mkdir buildroot-2011.08/dl
 cp ipshow-1.0.tar.gz buildroot-2011.08/dl/
