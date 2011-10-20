ef94ed686b59e4b78c1022770e935c99f75eac66
Martin d'Allens <caerbazar@gmail.com>
2011-10-20 20:30:50 +0200

    inclusion de minigui dans la config, et du patch uboot

--- Martin/chain/launch.sh
+++ Martin/chain/launch.sh
@@ -9,9 +9,13 @@ wget "http://buildroot.uclibc.org/downloads/$BUILDROOT.tar.gz" -O $BUILDROOT.tar
 tar -xf $BUILDROOT.tar.gz
 
 cp buildroot.config $BUILDROOT/.config
-cp -r dl $BUILDROOT/
+
+mkdir dl
+ln -s ../dl $BUILDROOT/dl
+cp minigui-0.1.tar.gz dl/
 cp -r minigui $BUILDROOT/package/
 patch $BUILDROOT/package/Config.in package-config.diff
+patch $BUILDROOT/boot/uboot/uboot.mk uboot.mk.diff
 
 cd $BUILDROOT
 
